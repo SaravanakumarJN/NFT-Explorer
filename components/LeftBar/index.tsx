96c598a31bb0733da04dea384d2e71f0e52e9b5f
Jaskaran <jaskaran.s@coinswitch.co>
2023-02-24 11:43:13 +0530

    wdf

--- components/LeftBar/index.tsx
+++ components/LeftBar/index.tsx
@@ -7,6 +7,9 @@ import NewPost from "../NewPost";
 import { useRouter } from "next/router";
 import { useAtom } from "jotai";
 import { user_atom } from "../stores/user.store";
+import LoginDialog from "../login-dialog";
+import { doc, getDoc } from "firebase/firestore";
+import { firebaseDB } from "../../firebase.cofig";
 
 const MainCon = styled.div``;
 
@@ -48,7 +51,25 @@ const rounts = [
 const LeftBar = ({ createNFT }: any) => {
   const router = useRouter();
   const [uplaodClicked, setuplaodClicked] = useState(false);
-  const [_, setUserData] = useAtom(user_atom);
+  const [userData, setUserData] = useAtom(user_atom);
+  const [login_error, setLoginError] = useState("");
+  const [is_login_loading, setIsLoginLoading] = useState(false);
+
+  const getUserDetails = async (phone_number) => {
+    setIsLoginLoading(true);
+    const userRef = doc(firebaseDB, "users", `${phone_number}`);
+    const user = await getDoc(userRef);
+
+    if (user?.exists()) {
+      const user_details = await (await getDoc(userRef)).data();
+      console.log("userRef", userRef, user_details);
+      localStorage.setItem("my-login", JSON.stringify(user_details));
+      setUserData(user_details);
+    } else {
+      setLoginError("User Doesn't Exists");
+    }
+    setIsLoginLoading(false);
+  };
 
   return (
     <MainCon className="h-[100vh] px-8 py-10 flex flex-col justify-between gradiant">
@@ -88,9 +109,8 @@ const LeftBar = ({ createNFT }: any) => {
         </div>
       </div>
       <FooterCon className="">
-       
         <div className="m-4 opacity-50 hover:opacity-100">
-          <div className="border-[1px] border-black w-fit px-5 py-2 rounded-3xl">
+          <div className="border-[1px] border-black w-fit px-10 py-2 rounded-3xl">
             <button
               className="flex items-center gap-2 font-medium"
               onClick={() => setuplaodClicked(true)}
@@ -100,17 +120,28 @@ const LeftBar = ({ createNFT }: any) => {
           </div>
         </div>
         <div
-          className="rounded-3xl flex justify-center font-bold py-5 px-4 bg-white"
+          className={`rounded-3xl flex justify-center font-bold py-3 cursor-pointer px-4 ${
+            userData ? "bg-[#f02424]" : "bg-[#1e5cef]"
+          } text-white`}
           onClick={() => {
             localStorage.clear();
             setUserData(null);
           }}
         >
-          LogOut
+          {userData ? "LogOut" : "Login"}
         </div>
       </FooterCon>
       {uplaodClicked && (
         <NewPost createNFT={createNFT} setuplaodClicked={setuplaodClicked} />
+      )}
+      {!userData && (
+        <LoginDialog
+          handleCloseLoginDialog={() => {}}
+          getUserDetails={getUserDetails}
+          login_error={login_error}
+          setLoginError={setLoginError}
+          is_login_loading={is_login_loading}
+        />
       )}
     </MainCon>
   );
