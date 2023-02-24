62ac51fc24b3894c178393d3184fea0406f4a53e
Chiranjeev Thapliyal <chiranjeev.t@coinswitch.co>
2023-02-24 09:10:45 +0530

    fix: added nav-link

--- components/LeftBar/index.tsx
+++ components/LeftBar/index.tsx
@@ -72,7 +72,8 @@ const LeftBar = ({ createNFT }: any) => {
               >
                 <h2
                   className={`font-semibold text-sm ${
-                    router.pathname === `/${route}`
+                    router.pathname === `/${route}` ||
+                    (name === "Home" && router.pathname === `/`)
                       ? "text-[#3b68fe]"
                       : "text-[#a2adc0]"
                   } `}
