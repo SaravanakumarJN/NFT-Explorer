a7bb7eff5e34ac0f7245eec42faf40d8058e188e
Jaskaran <jaskaran.s@coinswitch.co>
2023-02-24 11:26:55 +0530

    hg

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,9 +1,9 @@
-import { useAtom } from "jotai";
+import { collection, getDocs } from "firebase/firestore";
 import { useRouter } from "next/router";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Layout from "../../components/Layout";
-import { posts_data } from "../../components/stores/user.store";
+import { firebaseDB } from "../../firebase.cofig";
 
 const ImageBanner = styled.div<{ image: string }>`
   background-image: url(${({ image }) => image});
@@ -15,14 +15,31 @@ const ImageBanner = styled.div<{ image: string }>`
 
 const Post = () => {
   const router = useRouter();
-  const [postsData, setPostsData] = useAtom(posts_data);
   const { id } = router.query;
-  const data = postsData?.filter(({ node }) => {
-    return node.id === id;
-  })[0]?.node;
+  const [data, setData] = useState([]);
 
-  console.log(data);
-  const image = `https://dl.openseauserdata.com/cache/originImage/files/${data?.banner?.split("files/")?.[1]?.split("?")?.[0]}`
+  const getPostsData = async () => {
+    const postRef = collection(firebaseDB, "nftCat");
+    const posts = await getDocs(postRef);
+    let data: any = [];
+    posts.forEach((doc) => {
+      const d = doc.data();
+      data = [...data, ...d.data];
+    });
+    console.log(data, "sdkjjk");
+    setData(
+      data?.filter(({ node }) => {
+        return node.id === id;
+      })[0]?.node
+    );
+  };
+  useEffect(() => {
+    if (id) getPostsData();
+  }, [id]);
+
+  const image = `https://dl.openseauserdata.com/cache/originImage/files/${
+    data?.banner?.split("files/")?.[1]?.split("?")?.[0]
+  }`;
   return (
     <Layout>
       <div className="w-full h-[40%]">
@@ -33,17 +50,21 @@ const Post = () => {
           />
         </div>
         <div className="flex items-start justify-between px-4">
-
-        <div className="ml-3 w-fit flex gap-3">
-          <img src={image} className="w-[30%] rounded-full translate-y-[-50%] aspect-square object-cover" />
-          <div>
-          <h1 className="mt-2 text-2xl font-bold">{data.name}</h1>
-          <h3 className="mt-2 text-sm text-gray-500 font-semibold">{data.slug}</h3>
+          <div className="ml-3 w-fit flex gap-3">
+            <img
+              src={image}
+              className="w-[30%] rounded-full translate-y-[-50%] aspect-square object-cover"
+            />
+            <div>
+              <h1 className="mt-2 text-2xl font-bold">{data?.name}</h1>
+              <h3 className="mt-2 text-sm text-gray-500 font-semibold">
+                {data?.slug}
+              </h3>
+            </div>
           </div>
-        </div>
-        <button className="bg-[#3772ff] mt-2 py-2 px-7 cursor-pointer text-white font-bold text-sm rounded-full">
-                Buy
-              </button>
+          <button className="bg-[#3772ff] mt-2 py-2 px-7 cursor-pointer text-white font-bold text-sm rounded-full">
+            Buy
+          </button>
         </div>
       </div>
     </Layout>
